85e0372e9dea47141f9351aaaafec4c888c64e92
Diaa Mohammed <diaam852@gmail.com>
2024-04-01 01:33:16 +0200

    build

--- models/DBconnect.js
+++ models/DBconnect.js
@@ -1,6 +1,6 @@
 const mongoose =require("mongoose");
+require("dotenv").config()
 
-
-mongoose.connect("mongodb://127.0.0.1:27017/Library_Apis")
+mongoose.connect(process.env.URL)
         .then(() => console.log('Connected succesfully ....!'))
         .catch((err) => console.log(err));
